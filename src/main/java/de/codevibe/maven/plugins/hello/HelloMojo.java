1c1c7e8956992995e069a7bf70fcf675e12f43e9
Thomas Auinger <thomas@auinger.de>
2023-10-03 10:08:03 +0200

    added MavenProject field and property

--- src/main/java/de/codevibe/maven/plugins/hello/HelloMojo.java
+++ src/main/java/de/codevibe/maven/plugins/hello/HelloMojo.java
@@ -3,11 +3,22 @@ package de.codevibe.maven.plugins.hello;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 @Mojo(name = "hello")
 public class HelloMojo extends AbstractMojo {
 
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    MavenProject project;
+
+    @Parameter(property = "showRoots")
+    boolean showRoots;
+
     public void execute() throws MojoExecutionException {
         System.out.println("Hello!!!");
+        if (showRoots) {
+            System.out.println(this.project.getCompileSourceRoots());
+        }
     }
 }
