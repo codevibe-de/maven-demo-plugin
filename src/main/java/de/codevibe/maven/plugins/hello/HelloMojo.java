6cfa883b3f7292d831222c1c25748ccb79c7fa0e
Thomas Auinger <thomas@auinger.de>
2023-10-03 08:37:09 +0200

    Genesis

---
+++ src/main/java/de/codevibe/maven/plugins/hello/HelloMojo.java
@@ -0,0 +1,13 @@
+package de.codevibe.maven.plugins.hello;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "hello")
+public class HelloMojo extends AbstractMojo {
+
+    public void execute() throws MojoExecutionException {
+        System.out.println("Hello!!!");
+    }
+}
