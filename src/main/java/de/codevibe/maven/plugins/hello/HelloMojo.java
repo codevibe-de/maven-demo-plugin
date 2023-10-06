17c52f3b2bccd6f1c6d29972600071953f1ae192
Thomas Auinger <thomas@auinger.de>
2023-10-06 18:04:34 +0200

    minor changes / doc

--- src/main/java/de/codevibe/maven/plugins/hello/HelloMojo.java
+++ src/main/java/de/codevibe/maven/plugins/hello/HelloMojo.java
@@ -2,18 +2,19 @@ package de.codevibe.maven.plugins.hello;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-@Mojo(name = "hello")
+@Mojo(name = "hello", defaultPhase = LifecyclePhase.VALIDATE) // 'name' is name of goal
 public class HelloMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     MavenProject project;
 
     @Parameter(property = "showRoots")
-    boolean showRoots;
+    boolean showRoots = false;
 
     public void execute() throws MojoExecutionException {
         System.out.println("Hello!!!");
